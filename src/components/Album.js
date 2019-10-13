9e78009d1f49b980538f658490b64e5b6bd4e0c3
erri120 <erri120@protonmail.com>
2019-10-13 19:59:05 +0200

    Added more cards examples

--- src/components/Album.js
+++ src/components/Album.js
@@ -7,10 +7,24 @@ export class Album extends Component{
     cards: [
       {
         id: 1,
-        Title: 'Test',
-        Image: 'https://i.ytimg.com/vi/5_j_ilXDFxY/maxresdefault.jpg',
+        Title: 'Lexy\'s LOTD for Wabbajack',
+        Image: 'https://i.ytimg.com/vi/Sk_ejbczO28/hqdefault.jpg',
         ImageTitle: 'Hello There',
-        Description: 'Very nice indeed'
+        Description: 'Something'
+      },
+      {
+        id: 2,
+        Title: 'Unofficial Bevilex Autoinstaller',
+        Image: 'https://i.ytimg.com/vi/olcx0zqXszk/maxresdefault.jpg',
+        ImageTitle: 'Hello There',
+        Description: 'Something #2'
+      },
+      {
+        id: 3,
+        Title: 'Unofficial Viva New Vegas Installer',
+        Image: 'https://ohmy.disney.com/wp-content/uploads/2014/03/Deep-Thoughts-with-Kronk-Self-Affirmation.png',
+        ImageTitle: 'Hello There',
+        Description: 'Something #3'
       }
     ]
   }
