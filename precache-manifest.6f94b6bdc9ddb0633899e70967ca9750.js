55937b033fa3cbadccafe9630d1fb63ddbb9594a
erri120 <erri120@protonmail.com>
2019-10-13 17:24:31 +0200

    Updates

---
+++ precache-manifest.6f94b6bdc9ddb0633899e70967ca9750.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3660918cb7e001bd70368e79e77e4e9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d985aff0065513525df8",
+    "url": "/static/css/main.76673be4.chunk.css"
+  },
+  {
+    "revision": "b906c499703df4bfdc3d",
+    "url": "/static/js/2.71f9acfa.chunk.js"
+  },
+  {
+    "revision": "d985aff0065513525df8",
+    "url": "/static/js/main.c5a7ce98.chunk.js"
+  },
+  {
+    "revision": "f8fb2550dc32f65638b9",
+    "url": "/static/js/runtime-main.5778a745.js"
+  }
+]);
