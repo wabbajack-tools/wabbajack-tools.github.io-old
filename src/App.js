6c460727d0f68f9bdcf96a3f84206ab7d9f88313
erri120 <erri120@protonmail.com>
2019-10-13 20:11:26 +0200

    Fixed css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
 import Copyright from './components/Copyright';
 import Album from './components/Album';
+import { CssBaseline, makeStyles } from '@material-ui/core';
 
-function App() {
+export class App extends Component {
+  classes = makeStyles(theme => ({
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(6)
+    }
+  }));
+  render(){
   return (
-    <div className="App">
+    <React.Fragment>
+    <CssBaseline/>
+    <main>
     <Album/>
-      <footer className="footer">
+      <footer className={this.classes.footer}>
       <Copyright/>
       </footer>
-    </div>
+    </main>
+    </React.Fragment>
   );
+  }
 }
 
 export default App;
