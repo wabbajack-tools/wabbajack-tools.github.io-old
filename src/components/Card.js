8a94c5508fc1f0055e2400c9aaef5eba05fdd952
erri120 <erri120@protonmail.com>
2019-10-13 19:16:53 +0200

    Created Card component

---
+++ src/components/Card.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Link, Typography, makeStyles, CardMedia, CardContent, CardActions } from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  cardMedia: {
+    paddingTop: '56.25%' // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  }
+}));
+
+export class Card extends Component{
+  classes = useStyles();
+  render(){
+    return(
+      <Grid item key={this.props.key} xs={12} sm={6} md={4}>
+        <Card className={this.classes.card}>
+          <CardMedia
+            className={this.classes.cardMedia}
+            image={this.props.imageURL}
+            title={this.props.imageTitle}/>
+          <CardContent className={this.classes.cardContent}>
+            <Typography gutterBottom variant="h5" component="h2">
+              {this.props.cardTitle}
+            </Typography>
+            <Typography>
+              {this.props.cardDescription}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" color="primary">
+              View
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    );
+  }
+}
+
+Card.propTypes = {
+  card: PropTypes.object.isRequired
+};
+
+export default Card;
