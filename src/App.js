470508fc486352d133b5b456a8e4aed62d16eb25
erri120 <erri120@protonmail.com>
2019-10-14 13:51:23 +0200

    Service worker will register in production environment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Copyright from './components/Copyright';
 import Album from './components/Album';
 import globalStyle from './assets/components/globalStyle';
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
 import AppBarComponent from './components/AppBarComponent';
 
 export class App extends Component {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if(process.env.NODE_ENV === "production"){
+  serviceWorker.register();
+}
