224a2db5c442dbd49d19271e4d46f485174e53c4
erri120 <erri120@protonmail.com>
2019-10-14 13:56:52 +0200

    Created modlistState

--- src/components/Album.js
+++ src/components/Album.js
@@ -2,33 +2,9 @@ import React, { Component } from 'react';
 import { Grid, Container, Typography } from '@material-ui/core';
 import CardComponent from './CardComponent';
 import albumStyle from "../assets/components/albumStyle";
+import modlistState from '../assets/states/modlistState';
 
 export class Album extends Component{
-  state = {
-    cards: [
-      {
-        id: 1,
-        Title: 'Lexy\'s LOTD for Wabbajack',
-        Image: 'https://i.ytimg.com/vi/Sk_ejbczO28/hqdefault.jpg',
-        ImageTitle: 'Hello There',
-        Description: 'Something'
-      },
-      {
-        id: 2,
-        Title: 'Unofficial Bevilex Autoinstaller',
-        Image: 'https://i.ytimg.com/vi/olcx0zqXszk/maxresdefault.jpg',
-        ImageTitle: 'Hello There',
-        Description: 'Something #2'
-      },
-      {
-        id: 3,
-        Title: 'Unofficial Viva New Vegas Installer',
-        Image: 'https://ohmy.disney.com/wp-content/uploads/2014/03/Deep-Thoughts-with-Kronk-Self-Affirmation.png',
-        ImageTitle: 'Hello There',
-        Description: 'Something #3'
-      }
-    ]
-  }
   render(){
     return(
       <div>
@@ -41,7 +17,7 @@ export class Album extends Component{
       </div>
       <Container className={albumStyle.cardGrid} maxWidth="md">
       <Grid container spacing={4}>
-      {this.state.cards.map(card => {
+      {modlistState.cards.map(card => {
         return (
           <CardComponent
             key={card.id}
