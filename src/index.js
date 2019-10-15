7b1c7842e3b5dd4f4835ec7c7f46a74b8b3d3bae
erri120 <erri120@protonmail.com>
2019-10-15 12:17:19 +0200

    Added node-sass

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './assets/scss/wabbajack.scss';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 if(process.env.NODE_ENV === "production"){
