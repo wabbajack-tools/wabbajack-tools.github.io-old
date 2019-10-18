96424b3ab1650e130f2d4503c45dd9787e34bd19
erri120 <erri120@protonmail.com>
2019-10-18 17:07:32 +0200

    Redone Modlist Gallery

--- src/assets/js/sections/modlistGalleryStyle.js
+++ src/assets/js/sections/modlistGalleryStyle.js
@@ -1,5 +1,20 @@
-const modlistGalleryStyle = {
+import { title } from './../globalStyle';
 
+const modlistGalleryStyle = {
+  section: {
+    padding: '70px 0',
+    textAlign: 'center'
+  },
+  title: {
+    ...title,
+    marginBottom: '1rem',
+    marginTop: '30px',
+    minHeight: '32px',
+    textDecoration: 'none'
+  },
+  left: {
+    textAlign: 'left'
+  }
 }
 
 export default modlistGalleryStyle;
--- src/sections/ModlistGallery.js
+++ src/sections/ModlistGallery.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import clsx from 'clsx';
+import axios from 'axios';
 
 import { makeStyles } from '@material-ui/core/styles';
+
+import Grid from '@material-ui/core/Grid';
+
+import CardGallery from './../components/Card/CardGallery';
 
 import style from './../assets/js/sections/modlistGalleryStyle';
 
 const useStyles = makeStyles(style);
+const _ = require('underscore');
 
 export default function ModlistGallery() {
   const classes = useStyles();
-  return(<div>qwdawd</div>);
+  let modlists = {};
+  if (process.env.NODE_ENV === "production"){
+    axios
+      .get('https://raw.githubusercontent.com/wabbajack-tools/wabbajack-tools.github.io/code/src/assets/states/modlistState.json')
+      .then(res => {modlists = res.data});
+  }else{
+    modlists = require('./../assets/states/modlistState.json');
+  }
+  return(
+    <div className={classes.section}>
+      <h2 className={classes.title}>Featured Modlists</h2>
+      <Grid
+        container
+        direction="row"
+        spacing={3}
+        justify="space-around"
+        alignItems="center">
+
+        {_.map(modlists, (modlist) => {
+          {console.log(modlist)}
+          return (
+            <Grid item xs={12} sm={12} md={5}>
+              <CardGallery modlist={modlist}/>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </div>
+  );
 }
