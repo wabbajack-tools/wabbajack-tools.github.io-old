7c17e8fb75a413ad38af26af3b8022d51debf0b0
erri120 <erri120@protonmail.com>
2019-10-19 14:31:18 +0200

    Created modlist info page

--- src/assets/js/sections/modlistInfoStyle.js
+++ src/assets/js/sections/modlistInfoStyle.js
@@ -1,5 +1,39 @@
-const modlistInfoStyle = {
+import { title, defaultFont } from '../globalStyle';
 
+const modlistInfoStyle = {
+  section: {
+    padding: '70px 0'
+  },
+  title: {
+    ...title,
+    marginBottom: '1rem',
+    marginTop: '30px',
+    minHeight: '32px',
+    textDecoration: 'none',
+    color: '#03DAC6'
+  },
+  description: {
+    marginTop: '16px'
+  },
+  readme: {
+    marginTop: '16px'
+  },
+  image: {
+    width: '100%'
+  },
+  chips: {
+    marginTop: '10px',
+    display: "flex"
+  },
+  divider: {
+    marginTop: '12px',
+    background: 'rgba(255, 255, 255, 0.4)'
+  },
+  chip: {
+    background: '#3700B3',
+    color: 'white',
+    marginLeft: '4px'
+  }
 }
 
 export default modlistInfoStyle;
--- src/sections/ModlistInfo.js
+++ src/sections/ModlistInfo.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import axios from 'axios';
+import uuid from 'uuid';
+import Markdown from 'markdown-to-jsx';
 import PropTypes from 'prop-types';
 
 import { makeStyles } from '@material-ui/core/styles';
+
+import Chip from '@material-ui/core/Chip';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
 
 import styles from './../assets/js/sections/modlistInfoStyle';
 
@@ -14,6 +20,7 @@ export default function ModlistInfo(props){
   const classes = useStyles();
   const machineURL = props.match.params.url;
   let modlists = {};
+  const [readmeMD, setReadmeMD] = useState("NOTFOUND");
   if (process.env.NODE_ENV === "production"){
     axios
       .get('https://raw.githubusercontent.com/wabbajack-tools/wabbajack-tools.github.io/code/src/assets/states/modlistState.json')
@@ -21,13 +28,52 @@ export default function ModlistInfo(props){
   }else{
     modlists = require('./../assets/states/modlistState.json');
   }
-  const modlist = _.findWhere(modlists, (current) => {
+  const modlist = _.find(modlists, (current) => {
     return current.links.machineURL === machineURL
   });
-  const { name, description, author, game, official, links } = modlist;
+  const { title, description, author, game, official, links } = modlist;
   const { image, readme, download } = links;
+  axios.get(readme)
+  .then(res => {
+    setReadmeMD(res.data);
+  });
+  let gameName = game;
+  switch (game){
+    case 'skyrim': gameName = 'Skyrim'; break;
+    case 'oblivion': gameName = 'Oblivion'; break;
+    case 'skyrimspecialedition': gameName = 'Skyrim Special Edition'; break;
+    case 'fallout4': gameName = 'Fallout 4'; break;
+    case 'morrowind': gameName = 'Morrowind'; break;
+  }
   return(
-    <div>
+    <div className={classes.section}>
+    <h2 className={classes.title}>{title}</h2>
+    <img src={image} className={classes.image}/>
+    <Typography variant="caption">Created by {author}</Typography>
+    <div className={classes.chips}>
+        <Chip
+          key={uuid.v4()}
+          label={gameName}
+          className={classes.chip}
+          size="small"
+          />
+        {official ?
+          <Chip
+            key={uuid.v4()}
+            label="Official"
+            className={classes.chip}
+            size="small"/>
+          : (<React.Fragment/>)}
+      </div>
+      <Divider className={classes.divider}/>
+      <Typography variant="body1" className={classes.description}>{description}</Typography>
+      <Divider className={classes.divider}/>
+      {/*<div><Typography variant="body2" className={classes.readme}>{readmeMD}</Typography></div>*/}
+      <div>
+      <Markdown
+          children={readmeMD}
+          />
+      </div>
     </div>
   );
 }
