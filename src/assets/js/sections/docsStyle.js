ac63c3cd87928075a485399a9740fb77904501cd
erri120 <erri120@protonmail.com>
2019-10-21 16:33:08 +0200

    Added docs section

--- src/assets/js/sections/docsStyle.js
+++ src/assets/js/sections/docsStyle.js
@@ -1,5 +1,24 @@
-const docsStyle = {
+import { title, infoColor } from '../globalStyle';
 
+const docsStyle = {
+  section: {
+    padding: '70px 0',
+    textAlign: 'left'
+  },
+  title: {
+    ...title,
+    marginBottom: '1rem',
+    marginTop: '30px',
+    minHeight: '32px',
+    textDecoration: 'none'
+  },
+  secondTitle: {
+    color: infoColor
+  },
+  divider: {
+    marginTop: '12px',
+    background: 'rgba(255, 255, 255, 0.4)'
+  }
 }
 
 export default docsStyle;
--- src/sections/DocsSection.js
+++ src/sections/DocsSection.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import uuid from 'uuid';
 
 import { makeStyles } from '@material-ui/core/styles';
+
+import Typography from '@material-ui/core/Typography';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
 
 import style from './../assets/js/sections/docsStyle';
 
 const useStyles = makeStyles(style);
 
+function ListItemLink(props){
+  return <ListItem button component="a" {...props}/>;
+}
+
 export default function DocsSection(){
   const classes = useStyles();
   return(
-    <div></div>
+    <div className={classes.section}>
+    <h2 className={classes.title}>Documentation</h2>
+    <Typography variant="h5" className={classes.secondTitle}>Available Pages:</Typography>
+    <Divider className={classes.divider}/>
+    <div>
+      <ListItemLink href="/docs/faq" key={uuid.v4()}>
+        <ListItemText>FAQ</ListItemText>
+      </ListItemLink>
+      <ListItemLink href="/docs/guide" key={uuid.v4()}>
+      <ListItemText>Guides</ListItemText>
+    </ListItemLink>
+    </div>
+    <Divider className={classes.divider}/>
+    </div>
   )
 }
