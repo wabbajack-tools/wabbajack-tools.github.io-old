5efbde8c3879c28ff61be6b5e6e4d74e47124e41
erri120 <erri120@protonmail.com>
2019-10-21 14:46:04 +0200

    Changed modlist gallery from function to component class for better use with axios

--- src/sections/ModlistGallery.js
+++ src/sections/ModlistGallery.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import uuid from 'uuid';
+import PropTypes from 'prop-types';
 
 import { getGameName } from './../utils/Games';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 
 import Grid from '@material-ui/core/Grid';
 
@@ -17,46 +18,55 @@ import CardGallery from './../components/Card/CardGallery';
 
 import style from './../assets/js/sections/modlistGalleryStyle';
 
-const useStyles = makeStyles(style);
-const _ = require('underscore');
+import underscore from 'underscore';
 
-export default function ModlistGallery() {
-  const classes = useStyles();
-  let gamesCache = [];
-  const [values, setValues] = React.useState({
-    game: ''
-  });
-  const handleChange = event => {
-    setValues(oldValues => ({
-      ...oldValues,
-      [event.target.name]: event.target.value,
-    }));
-  }
-  let modlists = {};
-  axios
+class ModlistGallery extends Component {
+  state = {
+    modlists: [],
+    game: '',
+    gamesCache: []
+  };
+
+  componentDidMount() {
+    axios
     .get('https://raw.githubusercontent.com/wabbajack-tools/mod-lists/master/modlists.json')
-    .then(res => {modlists = res.data});
-  return(
+    .then(res => {
+      this.setState({modlists: res.data}, () => {
+        let templist = [];
+        underscore.map(this.state.modlists, (modlist) => {
+          if(!templist.includes(modlist.game)){
+            templist.push(modlist.game);
+          }
+        });
+        this.setState({gamesCache: templist});
+      });
+    });
+  }
+
+  handleChange = event => {
+    this.setState({game: event.target.value});
+  }
+
+  render(){
+  const { classes } = this.props;
+  return (
     <div className={classes.section}>
       <h2 className={classes.title}>Featured Modlists</h2>
       <div className={classes.searchOptions}>
       <FormControl className={classes.formControl}>
         <Select
           className={classes.select}
-          value={values.game}
-          onChange={handleChange}
+          value={this.state.game}
+          onChange={this.handleChange}
           inputProps={{
             name: 'game',
             id: 'select-game'
           }}>
           <MenuItem value="">All</MenuItem>
-          {_.map(modlists, (modlist) => {
-            if(!gamesCache.includes(modlist.game)){
-              gamesCache.push(modlist.game);
-              return (
-                <MenuItem value={modlist.game}>{getGameName(modlist.game)}</MenuItem>
-              );
-            }
+          {underscore.map(this.state.gamesCache, (gameInCache) => {
+            return(
+              <MenuItem value={gameInCache} key={uuid.v4()}>{getGameName(gameInCache)}</MenuItem>
+            );
           })}
         </Select>
         <FormHelperText className={classes.formControlText}>Filter by game</FormHelperText>
@@ -69,15 +79,22 @@ export default function ModlistGallery() {
         justify="space-around"
         alignItems="center">
 
-        {_.map(modlists, (modlist) => {
-          if(modlist.game === values.game || values.game === "")
-            return (
-              <Grid item xs={12} sm={12} md={5} key={uuid.v4()}>
-                <CardGallery modlist={modlist}/>
-              </Grid>
-            );
+        {underscore.map(this.state.modlists, (modlist) => {
+          if(modlist.game === this.state.game || this.state.game === "")
+          return (
+            <Grid item xs={12} sm={12} md={5} key={uuid.v4()}>
+              <CardGallery modlist={modlist}/>
+            </Grid>
+          );
         })}
       </Grid>
     </div>
   );
+  }
 }
+
+ModlistGallery.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(style)(ModlistGallery);
