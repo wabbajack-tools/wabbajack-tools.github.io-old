33ae2ecca57c8dfb7427b56368d885ba0d865138
erri120 <erri120@protonmail.com>
2019-12-12 19:23:59 +0100

    Removed service Worker import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { configureStore, history } from './store';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
 import './assets/scss/wabbajack.scss';
 
